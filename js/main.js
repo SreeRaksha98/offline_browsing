946603be270085b3466f8cc5c605b8409963bab6
Sreeraksha <raksharamesh1998@gmail.com>
2023-07-19 12:00:47 +0530

    filename correction

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@
 if('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
         console.log('App is loader');
-        navigator.serviceWorker.register('../sw_cached_pages.js')
+        navigator.serviceWorker.register('../sw.js')
             .then( () => {
                 console.log("Service Worker registerd");
             })
